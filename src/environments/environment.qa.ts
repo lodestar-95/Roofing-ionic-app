9958781cd2e706e1584270f9edb5d7c731c5547c
Lodestar <122153472+lodestar-95@users.noreply.github.com>
2026-02-23 04:58:25 -0600

    Update environment.qa.ts

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -6,24 +6,10 @@ export const environment = {
   production: true,
   name: 'qa',
    url: 'https://ehroofing-gw-gateway-qa-b1b593482770.herokuapp.com/api',
-  //url: 'http://localhost:8000/api',
-  //msJiraURl: 'https://eh-roofing-ms-jira-439f033cf179.herokuapp.com/',
-
-  /** dropbox keys */
-  //appKey:"57lwsplp5auk44k",
-  //appSecret:"0nzbfv5z7j5fdin",
-  //refreshToken: "Z4H5zFAg7yUAAAAAAAAAAdQOd00n0gisv-6vKvLuQjDpN1-T36joeLqPGaAoeFFe",
-
-  //appKey:"bmcndlzkv83uina",
-  //appSecret:"1gz4tozto32djyr",
-  //refreshToken: "KcJhVPxutlwAAAAAAAAAAS7xYPGSg8_k9VGlilxNZeDKTCdEHkfa1u68_Vkygm-f",
 
   appKey:"qgm8gkydey74u6g",
   appSecret:"doxew38mu5q2mlk",
   refreshToken: "3EOAmzFZO1oAAAAAAAAAAT4QQUtWutr3nM8T6qz8zk2BLmWsL4yGc7tfWgc6Vfz1",
-
-  //jiraUsername:"sebastiansaenz.94@gmail.com",
-  //jiraPassword:"ATATT3xFfGF0DBfefWEdC8qdG6uwVi29fFlauBYSNcIuuHuxJ5iSenpP8fIENgpfhr16eTOSSb_FrpIYuqDbaqybI_ZvgUH_7Mf01tGUuFWKxke8aRHBO3ylhCxNd0oss8K-Cf3YHDJCiwt5JvHVA07T5eiGFzKWoXjkAmLt3GEE9TGZGjYmnB0=DC729FA1"
 
   jiraUsername:"albertozarate@gmail.com",
   jiraPassword:"Roofing+1m3"
